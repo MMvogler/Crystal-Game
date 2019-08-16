e13adf49520d8302774fabde8d909cf31e24d452
Megan Vogler <mmphilmon@gmail.com>
2019-08-15 20:31:18 -0500

    working buttons

---
+++ assets/javascript/game.js
@@ -0,0 +1,62 @@
+$(document).ready(function() { 
+
+    // Variables for the number of wins and losses in the game
+    var wins = 0;
+    var losses = 0;
+
+    // Variable that reflects the users progress 
+    var counter = 0;
+
+    // Variables for where in HTML things need to go
+    var winsText = document.getElementById("wins-text");
+    var lossesText = document.getElementById("losses-text");
+    var counterText = document.getElementById("counter-text");
+
+    // A number is chosen between 19 - 120 for the user to match
+    var numberChoices = Math.floor(Math.random() * 120) + 20;
+    console.log(numberChoices);
+    numberChoicesText = document.getElementById("randomNumber");
+    
+
+    // Each crystal is given a value betwen 1 - 12 that changes after each game 
+    var crystalNumber = Math.floor(Math.random() * 12) + 1; 
+        $(".crystal").click(function() {
+            if(parseInt(this.value) === -1) {
+                this.value = Math.floor(Math.random() * 12) +1;
+            }
+            // Every click from every crystal adds to global counter
+            counter += parseInt(this.value);
+
+            // If the user wins
+        if(counter === numberChoices) {
+            wins++;
+
+        
+        // If the user looses
+        } else if (counter > numberChoices) {
+            losses++;
+
+        }
+
+
+        numberChoicesText.textContent = numberChoices;
+        winsText.textContent = "Wins: " + wins;
+        lossesText.textContent = "Losses: " + losses;
+        counterText.textContent = "Your total score is: " + counter;
+
+        })
+
+    
+    
+
+        numberChoicesText.textContent = numberChoices;
+        winsText.textContent = "Wins: " + wins;
+        lossesText.textContent = "Losses: " + losses;
+        counterText.textContent = "Your total score is: " + counter;
+
+    
+        
+        
+    
+});
+  
